11499c1a9d12fca4fb8fc5dc8297df024c0eb8dd
Alex Bates <ajbates93@gmail.com>
2022-10-12 10:41:28 +0100

    feat: update vite config with new packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetUno, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
+      ]
+    })
+  ]
 })
