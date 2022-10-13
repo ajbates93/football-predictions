67f48c075c3ef719a4e5f128b6d7c44e639c9588
Alex Bates <ajbates93@gmail.com>
2022-10-13 16:49:34 +0100

    feat: style updates to fixtures

--- src/components/Fixture.vue
+++ src/components/Fixture.vue
@@ -33,14 +33,15 @@ if (first)
 
 <template>
   <p mt5 mb1 text-xl v-if="showDate">{{new Date(fixture.fixture.date).toLocaleDateString()}}</p>
-  <div grid style="grid-template-columns: 3fr 1fr 3fr">
+  <div mb1 grid style="grid-template-columns: 3fr 1fr 3fr">
     <span px1 text-right inline-block>{{fixture.teams.home.name}}</span>
     <span grid-col-a text-center px1 inline-block>v</span> 
     <span px1 text-left inline-block>{{fixture.teams.away.name}}</span>
   </div>
-  <div v-if="showPrediction">
-    <input type="number" v-model="homePrediction" />
-    <span></span>
-    <input type="number" v-model="awayPrediction" />
+  <div v-if="showPrediction"
+    grid mb2 style="grid-template-columns: 3fr 1fr 3fr">
+    <input inputmode="numeric" rounded-sm justify-self-end mx1 inline-block w-10 text-center v-model="homePrediction" />
+    <span text-center px1 inline-block></span>
+    <input inputmode="numeric" rounded-sm justify-self-start mx1 inline-block w-10 text-center v-model="awayPrediction" />
   </div>
 </template>
